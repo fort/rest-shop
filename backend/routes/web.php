f7aef92091c892fe9a6ff9c5daa83ba777346215
tim <tevtushok@ukr.net>
2018-12-16 17:29:55 +0200

    add <client> api prefix

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -11,18 +11,26 @@
 |
 */
 
+const RESTSHOP_API = [
+	'version' => 1.0,
+	'description' => 'Opencart REST API'
+];
+
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+	return RESTSHOP_API;
 });
 
-$router->post('/auth/login', 'AuthController@postLogin');
 
-$router->group(
-    ['middleware' => 'auth:api'], 
-    function() use ($router) {
-        $router->get('/me', function() {
-            $user = Auth::user();
-            return response()->json($user);
-        });
-    }
-);
+
+$router->group(['prefix' => 'client'], function () use ($router) {
+	$router->post('/auth/login', 'AuthController@postLogin');
+	$router->group(
+		['middleware' => 'auth:client-api'], 
+		function() use ($router) {
+			$router->get('/me', function() {
+				$user = Auth::user();
+				return response()->json($user);
+			});
+		}
+	);
+});
