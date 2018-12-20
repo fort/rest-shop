b33d0d265bf6e1881865e9fa334ad75484581d48
tim <tevtushok@ukr.net>
2018-12-20 19:06:28 +0200

    clear some shit

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-
+use App\Models\Language;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
